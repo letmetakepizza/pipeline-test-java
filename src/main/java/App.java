c4dcb1c51fc8dcf44825245be4a3d3a590df1c4d
j1 <meansibleme@example>
2024-03-06 15:59:57 -0700

    initial java sample

---
+++ src/main/java/App.java
@@ -0,0 +1,15 @@
+/*
+ Basic Java app for CI/CD demo with Jenkins
+ Prints "Hello World!"
+*/
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+	public int placeholderMethod() {
+		return 0;
+	}
+}
